5849944ad22dbe15339a4d199bc91a163a5c705b
xxr_Indra <158313356+xxrindra@users.noreply.github.com>
2024-03-08 07:56:22 -0700

    Added in EXO v1.0

---
+++ exo/unpatch.py
@@ -0,0 +1,21 @@
+import os
+os.system('cls')
+import time
+print("Exoskeleton Client BETA RBLX UNPATCH")
+print("Reading Exoskeleton Client's Key")
+time.sleep(5)
+print("Key Detected")
+f = open("key.txt", "r")
+txt=f.read()
+print(txt)
+print("Loading bin files")
+time.sleep(3)
+print("Locating ROBLOX file")
+print("Silently injecting DLL")
+time.sleep(3)
+api.dll = "error 172:missing dll"
+print(api.dll)
+time.sleep(2)
+print("adding to error log for unpatcher.")
+with open('crashlogsunpatch.txt', 'w') as f:
+    f.write("error 172 occured while unpatcher was running.")
